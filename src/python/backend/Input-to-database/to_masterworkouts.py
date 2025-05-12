376958f9e091208ccd12133bf95b69f957d9e8c5
Kyle Krebs <kak4294@rit.edu>
2025-05-12 00:42:01 -0400

    Stubbed out files for data movement into database

---
+++ src/python/backend/Input-to-database/to_masterworkouts.py
@@ -0,0 +1,8 @@
+"""
+Stubbed out file to take data given as input from the frontend & move that master 
+workoutout data to my database which is connected in the backend. This data being
+transfered can pertain to both endurance & resistance training. 
+
+Author: Kyle Krebs
+Date: 5/12/25
+"""
---
+++ src/python/backend/Input-to-database/to_resistanceworkouts.py
@@ -0,0 +1,7 @@
+"""
+Stubbed out file to take data given as input from the frontend & move that resistance 
+workoutout data to my database which is connected in the backend.
+
+Author: Kyle Krebs
+Date: 5/12/25
+"""
